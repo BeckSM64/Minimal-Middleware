b478f9d14154b3e480acb62231057aabd5a7e0f4
BeckSM64 <BeckSM64@gmail.com>
2025-10-11 22:01:43 -0400

    :sparkles: handle concurrent connections from clients in broker

--- broker/broker.cpp
+++ broker/broker.cpp
@@ -3,6 +3,8 @@
 #include <cstring>
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <mutex>
 
 #define PORT 5000
 #define BUFFER_SIZE 1024
@@ -14,51 +16,24 @@ struct ConnectedClient {
 };
 
 std::vector<ConnectedClient> connectedClientList;
+std::mutex clientListMutex;
 
-int main() {
-    int server_fd, client_fd;
-    struct sockaddr_in address;
-    socklen_t addrlen = sizeof(address);
+void handleClient(int client_fd) {
     char buffer[BUFFER_SIZE];
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        perror("socket"); return 1;
-    }
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        perror("bind");
-        return 1;
-    }
-
-    if (listen(server_fd, 5) < 0) {
-        perror("listen");
-        return 1;
-    }
-
-    std::cout << "Broker listening on port " << PORT << std::endl;
-
-    client_fd = accept(server_fd, (struct sockaddr*)&address, &addrlen);
-
-    // Check if client connects
-    if (client_fd < 0) {
-        perror("accept"); return 1;
-    }
-
-    // Add client to connected client list
+    // Add client to list (example data)
     ConnectedClient newClient;
-    newClient.ip_address = "127.0.0.1";
+    newClient.ip_address = "127.0.0.1"; // Replace with actual IP if needed
     newClient.type = "publisher";
     newClient.topic = "test topic";
 
-    connectedClientList.push_back(newClient);
-    std::cout << "Client connected!" << std::endl;
-    for (int i = 0; i < connectedClientList.size(); i++) {
-        std::cout << connectedClientList.at(i).topic << std::endl;
+    {
+        std::lock_guard<std::mutex> lock(clientListMutex);
+        connectedClientList.push_back(newClient);
+        std::cout << "Client list updated: " << std::endl;
+        for (int i = 0; i < connectedClientList.size(); i++) {
+            std::cout << " - " << connectedClientList[i].topic << std::endl;
+        }
     }
 
     while (true) {
@@ -73,6 +48,43 @@ int main() {
     }
 
     close(client_fd);
+    std::cout << "Client disconnected" << std::endl;
+}
+
+int main() {
+    int server_fd;
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(address);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) { perror("socket"); return 1; }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind"); return 1;
+    }
+
+    if (listen(server_fd, 5) < 0) {
+        perror("listen"); return 1;
+    }
+
+    std::cout << "Broker listening on port " << PORT << std::endl;
+
+    while (true) {
+        int client_fd = accept(server_fd, (struct sockaddr*)&address, &addrlen);
+        if (client_fd < 0) {
+            perror("accept");
+            continue;
+        }
+
+        std::cout << "Client connected!" << std::endl;
+        std::thread t(handleClient, client_fd);
+        t.detach(); // Let the client run independently
+    }
+
     close(server_fd);
     std::cout << "Broker exiting" << std::endl;
 }
