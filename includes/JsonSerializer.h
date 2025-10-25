19b4caf857d7051c2a2e6293adab03721435e1e8
BeckSM64 <becksm64@gmail.com>
2025-10-25 12:31:58 -0400

    :bug: add override to serializer interface functions

--- includes/JsonSerializer.h
+++ includes/JsonSerializer.h
@@ -6,6 +6,6 @@ class JsonSerializer : public IMmwMessageSerializer {
     public:
         std::string serialize(const MmwMessage& msg) override;
         std::string serialize_raw(const MmwMessage& msg) override;
-        MmwMessage deserialize(const std::string& data);
-        MmwMessage deserialize_raw(const std::string& data);
+        MmwMessage deserialize(const std::string& data) override;
+        MmwMessage deserialize_raw(const std::string& data) override;
 };
