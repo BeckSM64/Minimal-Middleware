be2da7af856d280c93155c5fe34d2a7665e26b88
BeckSM64 <becksm64@gmail.com>
2025-10-28 16:45:07 -0400

    :sparkles: get subscriber callback for string messages working

--- python/Bindings.cpp
+++ python/Bindings.cpp
@@ -3,6 +3,16 @@
 
 namespace py = pybind11;
 
+// This will hold the Python callback globally per subscriber (simplest approach)
+static py::function g_python_callback;
+
+extern "C" void subscriber_trampoline(const char* msg) {
+    py::gil_scoped_acquire acquire;  // Acquire GIL before calling Python
+    if (g_python_callback) {
+        g_python_callback(std::string(msg)); // Convert C string to Python str
+    }
+}
+
 PYBIND11_MODULE(mmw_python, m) {
     m.doc() = "Python bindings for Minimal Middleware";
 
@@ -18,4 +28,9 @@ PYBIND11_MODULE(mmw_python, m) {
     m.def("publish", &mmw_publish, py::arg("topic"), py::arg("message"));
     m.def("publish_raw", &mmw_publish_raw, py::arg("topic"), py::arg("message"), py::arg("size"));
     m.def("cleanup", &mmw_cleanup);
+
+    m.def("create_subscriber", [](const char* topic, py::function callback) {
+        g_python_callback = callback;
+        mmw_create_subscriber(topic, &subscriber_trampoline);
+    });
 }
