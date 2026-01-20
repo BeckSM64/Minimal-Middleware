4b9fef97c34289b06706e8ab3e3695a08f31a75c
BeckSM64 <becksm64@gmail.com>
2026-01-20 16:23:10 -0500

    :wrench: start fixing windows python whl build

--- python/mmw/__init__.py
+++ python/mmw/__init__.py
@@ -1,2 +1,24 @@
+# import os
+# from pathlib import Path
+
+# # Add MinGW bin dir first
+# os.add_dll_directory(r"C:\msys64\mingw64\bin")
+
+# # Add package folder for libmmw.dll
+# os.add_dll_directory(str(Path(__file__).parent))
+
+# from .mmw import *
+
+import os
+from pathlib import Path
+import sys
+
+# Only needed on Windows for DLL search paths
+if sys.platform.startswith("win"):
+    # Add MinGW runtime DLLs
+    os.add_dll_directory(r"C:\msys64\mingw64\bin")
+    # Add the folder containing libmmw.dll
+    os.add_dll_directory(str(Path(__file__).parent))
+
 from .mmw import *
 
